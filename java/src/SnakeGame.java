aa00625998319549c2758b9c4cfb6a1152f3df31
Rodrigo Mesta <rimesta@miners.utep.edu>
2020-02-22 15:21:28 -0700

    Finished findTailExhaustive and added resetCounters and getExhaustiveChecks

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -1,3 +1,5 @@
+import java.net.NetworkInterface;
+
 public class SnakeGame {
     private boolean[][] game;
     private int[] headPosition;
@@ -12,6 +14,7 @@ public class SnakeGame {
         headPosition = new int[2];
         headPosition[0] = x;
         headPosition[1] = y;
+        resetCounters();
 
         game = new boolean[G.length][G[0].length];
         for(int i = 0; i < G.length; i++) {
@@ -22,94 +25,57 @@ public class SnakeGame {
     }
 
     public int[] findTailExhaustive() {
-        System.out.println(headPosition[0] +" "+ headPosition[1]);
         int[] xyLength = new int[3];
         int length = 0;
-        exhaustiveChecks = 0;
+        int neighbors = 0;
+        int temp = 0;
         /* Am I part of the snake?
            Everytime you find a true you count
          */
-        for(int i = 0; i < game.length; i++){
-            for(int j = 0; j < game[i].length; j++){
-                exhaustiveChecks++;
-                if(game[i][j]) {
+        for(int i = 0; i < game.length; i++) {
+            for (int j = 0; j < game[i].length; j++) {
+                temp++;
+                if (i == headPosition[0] && j == headPosition[1]) {
                     length++;
-
-                    if(i == 0 && j == 0){
-                        if((game[0][1] || game[1][0]) && (i != headPosition[0] && j != headPosition[1]) ){
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        }
-                    } else if(i == game.length-1 && j == game[i].length-1){
-                        if((game[game.length][game[i].length - 2] || game[game.length - 2][game[i].length])
-                                && (i != headPosition[0] && j != headPosition[1])){
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        }
-                    } else if(i == 0 && j == game[i].length-1){
-                        if((game[0][game[i].length - 2] || game[1][game[i].length])
-                                && (i != headPosition[0] && j != headPosition[1])){
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        }
-                    } else if(i == game.length-1 && j == 0){
-                        if((game[game.length - 1][1] || game[game.length - 2][0])
-                                && (i != headPosition[0] && j != headPosition[1])){
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        }
-                    } else if(i-1 < 0) {
-                        if ((game[i][j + 1] && !game[i][j - 1] || !game[i][j + 1] && game[i][j - 1])
-                                && (i != headPosition[0] && j != headPosition[1])) {
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        } else if ((game[i + 1][j]) && (i != headPosition[0] && j != headPosition[1])) {
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        }
-                    } else if(j-1 < 0) {
-                        if ((game[i][j + 1]) && (i != headPosition[0] && j != headPosition[1])) {
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        } else if ((!game[i - 1][j] && game[i + 1][j] || game[i - 1][j] && !game[i + 1][j])
-                                && (i != headPosition[0] && j != headPosition[1])) {
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        }
-                    } else if(j+1 == game[i].length) {
-                        System.out.println("test");
-                        if ((game[i][j - 1]) && (i != headPosition[0] && j != headPosition[1])) {
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        } else if ((!game[i - 1][j] && game[i + 1][j] || game[i - 1][j] && !game[i + 1][j])
-                                && (i != headPosition[0] && j != headPosition[1])) {
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        }
-                    } else if(i+1 == game.length) {
-                        if ((game[i][j + 1] && !game[i][j - 1] || !game[i][j + 1] && game[i][j - 1])
-                                && (i != headPosition[0] && j != headPosition[1])) {
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        } else if ((game[i -1][j]) && (i != headPosition[0] && j != headPosition[1])) {
-                            xyLength[0] = i;
-                            xyLength[1] = j;
-                        }
-                    } else if (((game[i][j + 1] && !game[i][j - 1]) || (!game[i][j + 1] && game[i][j - 1]))
-                            && (i != headPosition[0] && j != headPosition[1])) {
-                        System.out.println(i + " " + j);
+                } else if (game[i][j]) {
+                    length++;
+                    neighbors = NeighborCount(i,j);
+                    if (neighbors == 1){
                         xyLength[0] = i;
                         xyLength[1] = j;
-                    } else if ((!game[i - 1][j] && game[i + 1][j] || game[i - 1][j] && !game[i + 1][j])
-                            && (i != headPosition[0] && j != headPosition[1])) {
-                        xyLength[0] = i;
-                        xyLength[1] = j;
+                        exhaustiveChecks = temp;
                     }
                 }
             }
         }
         xyLength[2] = length;
         return xyLength;
+    }
+
+    public int NeighborCount(int row, int col) {
+        int count = 0;
+        if((row - 1 > -1) && game[row -1][col]){
+            count++;
+        }
+        if((row + 1 < game.length) && game[row+1][col]) {
+            count++;
+        }
+        if((col - 1 > -1) && game[row][col - 1]) {
+            count++;
+        }
+        if((col + 1 < game[row].length) && game[row][col + 1] ) {
+            count++;
+        }
+        return count;
+    }
+
+    private void resetCounters() {
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
+    }
+
+    private static int getExhaustiveChecks() {
+        return exhaustiveChecks;
     }
 
 }
--- java/src/Test.java
+++ java/src/Test.java
@@ -1,17 +1,21 @@
 public class Test {
     public static void main(String[] arg){
         boolean[][] play = {
-                {false, false, false, false},
-                {false, true, true, true},
-                {false, false, false, false},
-                {false, false, false, false}
+                {true, true, true, true, true},
+                {true, false, false, false, true},
+                {true, false, true, false, true},
+                {true, false, true, false, true},
+                {true, true, true, false, true}
         };
 
-        SnakeGame test = new SnakeGame(play, 1, 1);
+        //line 74
+
+        SnakeGame test = new SnakeGame(play, 4, 4);
         int[] ans = new int[3];
         ans = test.findTailExhaustive();
         for(int i = 0; i < ans.length; i++){
             System.out.print(ans[i] + " ");
         }
+        //System.out.print(test.getExhaustiveChecks());
     }
 }
